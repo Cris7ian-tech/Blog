b13a8db74f857b2caae3faa70851a6906f2d81ee
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-10-30 04:17:51 -0300

    Button leer mas componente

---
+++ src/components/ui/ButtonLeerMas.tsx
@@ -0,0 +1,9 @@
+
+
+const ButtonLeerMas = () => {
+  return (
+    <button className="border border-cyan-400 px-4 py-2 rounded-full text-gray-400">Leer más</button>
+  )
+}
+
+export default ButtonLeerMas
