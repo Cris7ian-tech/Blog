120a1c32b6ea6f94564e1df80fe31e2916339312
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-10-30 01:43:04 -0300

    Footer Componente

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-900 text-gray-300 text-center py-4">
+    <footer className="bg-neutral-900 text-gray-300 text-center py-4">
         <p>
           © {new Date().getFullYear()} Mi Blog — Creado con{" "}
           <span className="text-blue-300">React</span> y{" "}
