48921c63ffdfc3dc1a8e54a64947ba43a6a160d0
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-05 13:48:35 -0300

    creacion de ButtonLeerMas

--- src/components/ui/ButtonLeerMas.tsx
+++ src/components/ui/ButtonLeerMas.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
 
 const ButtonLeerMas = () => {
+  const [expandida, setExpandida] = useState(true);
+
   return (
-    <button className="border border-cyan-400 font-semibold px-4 py-2 rounded-full text-gray-400 cursor-pointer hover:bg-cyan-400 hover:text-black">Leer más</button>
+    // <button className="border border-cyan-400 font-semibold px-4 py-2 rounded-full text-gray-400 cursor-pointer hover:bg-cyan-400 hover:text-black">Leer más</button>
+  <button 
+        onClick={() => setExpandida(!expandida)}
+        className="mt-4 text-sm font-bold text-cyan-400 hover:text-cyan-300 transition-colors">
+    {/*TERNARIO VA PALI?*/}
+    { expandida? "Leer más +" : "Leer menos -" }
+        </button>
   )
 }
 
