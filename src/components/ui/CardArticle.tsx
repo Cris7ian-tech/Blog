bc2bc6bde87ba28893b56d5e4341f630b689938b
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-05 13:45:47 -0300

    agregamos type CardArticleProps, integramos Compouds component "paragrah", saque boton ller mas

--- src/components/ui/CardArticle.tsx
+++ src/components/ui/CardArticle.tsx
@@ -1,36 +1,33 @@
 import { useState } from "react";
 
-import hooksData from "../../data/HooksData.json";
-import ButtonLeerMas from "./ButtonLeerMas";
+import Paragraph from "../Paragraph";
 
+type CardArticleProps = {
+  titulo: string;
+  subtitulo: string;
+  descripcion: string;
+};
 
-function CardArticle({ titulo, subtitulo, descripcion }) {
-
+function CardArticle({ titulo, subtitulo, descripcion }: CardArticleProps) {
   const [expandida, setExpandida] = useState(false);
-
   return (
     <>
 
         <article className="bg-neutral-900 shadow min-h-[200px] rounded-lg p-4 border border-stone-700
         transform transition-all duration-300 hover:translate-y-lg hover:shadow-cyan-500/20 hover:border-cyan-600">
           <div>
-            <h3 className="font-medium text-2xl text-neutral-300 mb-4 leading-5">{titulo}</h3>
-            <h4 className="font-semibold text-neutral-400 mb-4 leading-5">{subtitulo}</h4>
-              <p className="text-neutral-500 mb-4 leading-5 line-clamp-4">{descripcion}</p>
+              <Paragraph className="font-medium text-2xl text-neutral-300 mb-4 leading-5">
+                <h3 className="font-medium text-2xl text-neutral-300 mb-4 leading-5">{titulo}</h3>
+                <h4 className="font-medium text-xl text-neutral-500 mb-4 leading-5">{subtitulo}</h4>
+                {expandida ? descripcion : `${descripcion.slice(0, 100)}...`  }
+                <button
+                  className="text-cyan-400 ml-2 hover:underline"
+                  onClick={() => setExpandida(!expandida)}>
+                  {expandida ? 'Leer menos' : 'Leer más'}
+                </button>
+              </Paragraph>
           </div>
-          <ButtonLeerMas />
         </article>
-
-        <button 
-        onClick={() => setExpandida(!expandida)}
-        className="mt-4 text-sm font-bold text-cyan-400 hover:text-cyan-300 transition-colors">
-
-    {/*TERNARIO VA PALI?*/}
-    { expandida? "Leer más ▲" : "Leer menos ▼" }
-
-        </button>
-
-
     </>
   )
 }
