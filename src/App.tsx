9f08f5cef003121fec987d60ffcabdfe0cb36ca3
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-06 15:10:28 -0300

    prueba card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 
-import MainLayout from "./pages/MainLayout";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+
+import {Card, CardHeader, CardBody, CardFooter} from "./components/ui/Card.tsx";
+import MainLayout from "./pages/MainLayout.tsx";
+import Footer from "./components/Footer.tsx";
+import Header from "./components/Header.tsx";
 import "./index.css";
 
 function App() {
@@ -14,6 +16,32 @@ function App() {
 
       <main>
         <MainLayout />
+        <Card className="w-[480px] h-[220xp]">
+
+          <CardHeader>
+            <div className="bg-gray-400 font-semibold rounded-t-xl p-4">
+              <h2 className="text-xl font-bold text-white">Bienvenido/a a esta sección!</h2>
+            </div>
+          </CardHeader>
+
+          <CardBody  className="flex justify-between p-4">
+            <div className="p-2">
+              <p className="mt-4 text-sm text-gray-700 ">Quizas te interesa esto. Estamos aprendiendo React Hooks y compound components, seguramente esto este mal, pero por algo se comienza.</p>
+            </div>
+            <img 
+            className="w-[180px h-[156px] rounded-xl"
+            src="https://images.pexels.com/photos/34212916/pexels-photo-34212916.jpeg" alt="codigo"/>
+
+          </CardBody>
+
+          <CardFooter className="bg-gray-300 p-4 rounded-b-2xl">
+            <div className="flex justify-around">
+              <button className="border border-orange-300 rounded-full p-2 bg-orange-400 text-white">Leer Post</button>
+              <button><img src="/src/assets/share.svg" alt="compartir"/></button>
+            </div>
+          </CardFooter>
+
+        </Card>
       </main>
 
       <footer>
