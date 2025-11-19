17d861e171cb8d6d270d4fa7edf44c3276acc117
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-19 06:20:21 -0300

    Route funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,40 @@
+// import Projects from "./pages/Projects";
 
-
-import {Card, CardHeader, CardBody, CardFooter} from "./components/ui/Card.tsx";
+import ProyectosLayout from "./pages/Projects.tsx";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "./pages/MainLayout.tsx";
+import PokemonsGrid from "./components/PokemonsGrid.tsx";
+import PokemonLayout from "./pages/PokemonLayout.tsx";
 import Footer from "./components/Footer.tsx";
 import Header from "./components/Header.tsx";
 import "./index.css";
+import Contacto from "./pages/Contacto.tsx";
 
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header>
+      <div className="App">
+
         <Header />
-      </header>
-
-      <main>
-        <MainLayout />
-        <Card className="w-[480px] h-[220xp]">
-
-          <CardHeader>
-            <div className="bg-gray-400 font-semibold rounded-t-xl p-4">
-              <h2 className="text-xl font-bold text-white">Bienvenido/a a esta sección!</h2>
-            </div>
-          </CardHeader>
-
-          <CardBody  className="flex justify-between p-4">
-            <div className="p-2">
-              <p className="mt-4 text-sm text-gray-700 ">Quizas te interesa esto. Estamos aprendiendo React Hooks y compound components, seguramente esto este mal, pero por algo se comienza.</p>
-            </div>
-            <img 
-            className="w-[180px h-[156px] rounded-xl"
-            src="https://images.pexels.com/photos/34212916/pexels-photo-34212916.jpeg" alt="codigo"/>
-
-          </CardBody>
-
-          <CardFooter className="bg-gray-300 p-4 rounded-b-2xl">
-            <div className="flex justify-around">
-              <button className="border border-orange-300 rounded-full p-2 bg-orange-400 text-white">Leer Post</button>
-              <button><img src="/src/assets/share.svg" alt="compartir"/></button>
-            </div>
-          </CardFooter>
-
-        </Card>
-        
-      </main>
-      <div>
-        
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<MainLayout />} />
+          {/* Proyectos */}
+          <Route path="/projects" element={<ProyectosLayout />} />
+          {/* Lista de pokemons */}
+          <Route path="/pokemons" element={<PokemonsGrid />} />
+          {/* Página individual */}
+          <Route path="/pokemon/:id" element={<PokemonLayout />} />
+          {/* Contacto */}
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+          <Footer />
       </div>
-
-      <footer>
-        <Footer />
-      </footer>
-
-    </div>
     </>
   );
-} 
+}
 export default App;
+
+
+
