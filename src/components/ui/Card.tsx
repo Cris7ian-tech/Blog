4ba77a5578881d61ac64e501ace83e9dfd30d41e
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-19 01:24:14 -0300

    card modificada

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,69 +1,64 @@
 import type React from "react";
 
 /* COMPONENTE PADRE: CARD  */
-interface CardProps extends
-React.ComponentPropsWithoutRef<"div"> {
+interface CardProps extends React.ComponentPropsWithoutRef<"div"> {
   children: React.ReactNode;
   className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, className }) => {
+const Card: React.FC<CardProps> = ({ children, className, ...props }) => {
   return (
     <div
-      className={`bg-neutral-200 shadow min-h-[300px] rounded-2xl border border-stone-700 ${className}`}
+      {...props}
+      className={`bg-neutral-200 shadow min-h-[300px] rounded-2xl border border-stone-700 ${className || ""}`}
     >
       {children}
     </div>
   );
-}
+};
 
-/* COMPONENTE HIJO: CARDHEADER */
-interface CardHeaderProps extends
-React.ComponentPropsWithoutRef<"div"> {
+/* COMPONENTE HIJO: CARD HEADER */
+interface CardHeaderProps extends React.ComponentPropsWithoutRef<"div"> {
   children: React.ReactNode;
   className?: string;
 }
 
-const CardHeader : React.FC<CardHeaderProps> = ({ children, className }) => {
-  return <div className={`mb-4 ${className}`}>{children}</div>;
-}
+const CardHeader: React.FC<CardHeaderProps> = ({ children, className }) => {
+  return <div className={`mb-4 ${className || ""}`}>{children}</div>;
+};
 
 /* COMPONENTE HIJO: CARD BODY */
-interface CardBodyProps extends
-React.ComponentPropsWithoutRef<"div"> {
+interface CardBodyProps extends React.ComponentPropsWithoutRef<"div"> {
   children: React.ReactNode;
   className?: string;
 }
 
 const CardBody: React.FC<CardBodyProps> = ({ children, className }) => {
-  return <div className={`mb-4 ${className}`}>{children}</div>;
-}
+  return <div className={`mb-4 ${className || ""}`}>{children}</div>;
+};
 
 /* COMPONENTE HIJO: CARD IMAGE */
-interface CarImageProps extends
-React.ComponentPropsWithoutRef<"img"> {
-  children: React.ReactNode;
+interface CardImageProps extends React.ComponentPropsWithoutRef<"img"> {
   className?: string;
+}
 
 const CardImage: React.FC<CardImageProps> = ({ className, ...props }) => {
   return (
     <img
-    {...props}
-    className={`w-full h-40 object-contain rounded-t-2xl ${className}`}
+      {...props}
+      className={`w-full h-40 object-contain rounded-t-2xl ${className || ""}`}
     />
-    )
-  }
-}
+  );
+};
 
 /* COMPONENTE HIJO: CARD FOOTER */
-interface CardFooterProps extends
-React.ComponentPropsWithoutRef<"div"> {
+interface CardFooterProps extends React.ComponentPropsWithoutRef<"div"> {
   children: React.ReactNode;
   className?: string;
 }
 
 const CardFooter: React.FC<CardFooterProps> = ({ children, className }) => {
-  return <div className={`${className}`}>{children}</div>;
-}   
+  return <div className={`${className || ""}`}>{children}</div>;
+};
 
 export { Card, CardHeader, CardBody, CardFooter, CardImage };
