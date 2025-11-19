e92a74a67dd5a24b9d0b7b166449abfb274e9879
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-11-19 06:15:32 -0300

    pokehook

--- src/hooks/usePokemons.tsx
+++ src/hooks/usePokemons.tsx
@@ -19,7 +19,7 @@ interface UsePokemonsReturn {
   error: string | null;
 }
 
-export function usePokemons(limit: number = 120): UsePokemonsReturn {
+export function usePokemons(limit: number = 220): UsePokemonsReturn {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -60,65 +60,3 @@ export function usePokemons(limit: number = 120): UsePokemonsReturn {
   return { pokemons, loading, error };
 }
 
-
-// import { useState, useEffect } from "react";
-
-// interface PokemonBasic {
-//   name: string;
-//   url: string;
-// }
-
-// export interface Pokemon {
-//   id: number;
-//   name: string;
-//   image: string;
-//   types: string[];
-// }
-
-// interface UsePokemonsReturn {
-//   pokemons: Pokemon[];
-//   loading: boolean;
-//   error: string | null;
-// }
-
-// export default function usePokemons(limit: number = 120): UsePokemonsReturn {
-//   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchPokemons = async () => {
-//       try {
-//         const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
-//         const data = await res.json();
-//         console.log("BASIC LIST:", data.results);
-
-//         const basicPokemons: PokemonBasic[] = data.results;
-
-//         const detailed = await Promise.all(
-//           basicPokemons.map(async (poke) => {
-//             const pokeRes = await fetch(poke.url);
-//             const pokeData = await pokeRes.json();
-
-//             return {
-//               id: pokeData.id,
-//               name: pokeData.name,
-//               image: pokeData.sprites.other["official-artwork"].front_default,
-//               types: pokeData.types.map((t: any) => t.type.name),
-//             };
-//           })
-//         );
-
-//         setPokemons(detailed);
-//       } catch {
-//         setError("No se pudieron cargar los pokemones");
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchPokemons();
-//   }, [limit]);
-
-//   return { pokemons, loading, error };
-// }
